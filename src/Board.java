6eaefd38d16c51b17589dc453c7cbdc0ea0277d4
Rohan D'Souza <rodsouza9@gmail.com>
2016-04-09 01:32:11 -0700

    added more constructors

--- src/Board.java
+++ src/Board.java
@@ -17,9 +17,46 @@ public class Board {
             String[][] temp = new String[board.length][board[0].length];
             for (int boardIndex = 0; boardIndex < board.length; boardIndex++) {
                 for (int board2Index = 0; board2Index < board[0].length; board2Index++) {
-                    temp[boardIndex][board2Index] = board
+                    temp[boardIndex][board2Index] = board[boardIndex][board2Index];
                 }
+            }
+            this.board = temp;
+        }
+    }
+    public Board(short len) {
+        this.board = new String[len][len];
+    }
+
+    public void fill() {
+        for (int boardIndex = 0; boardIndex < board.length; boardIndex++) {
+            for (int board2Index = 0; board2Index < board[0].length; board2Index++) {
+                board[boardIndex][board2Index] = " ";
             }
         }
     }
+
+    /*public boolean isEmptyNotFilled() {
+        return board == null;
+    }*/
+    public boolean isEmpty() {
+        if (board == null) {
+            return true;
+        }
+        for (int boardIndex = 0; boardIndex < board.length; boardIndex++) {
+            for (int board2Index = 0; board2Index < board[0].length; board2Index++) {
+                if (board[boardIndex][board2Index] == null || board[boardIndex][board2Index].equals("")
+                    || board[boardIndex][board2Index].equals(" "))
+                    return true;
+            }
+        }
+        return false;
+    }
+    public boolean isEmpty(short num1, short num2) {
+        return board[num1][num2] == null;
+    }
+
+
+
+
+    
 }
---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rohan D'Souza on 4/9/2016.
+ */
+public class Tester {
+    public static void main(String[] args) {
+        Board board = new Board((short) 3);
+        board.fill();
+        System.out.print(board);
+    }
+}
