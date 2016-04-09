21bdfb6b60471d6b6bba04bd21b5876c40af0878
Rohan D'Souza <rodsouza9@gmail.com>
2016-04-08 17:18:05 -0700

    added default constuctor to Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,25 @@
+import java.util.IllegalFormatException;
+
 /**
  * Created by Rohan D'Souza on 4/8/2016.
  */
 public class Board {
-    public
+    public String[][] board;
+
+    public Board() {
+        this.board = null;
+    }
+    public Board(String[][] board) {
+        if (board == null || board[0] == null) {
+            this.board = null;
+        }
+        else {
+            String[][] temp = new String[board.length][board[0].length];
+            for (int boardIndex = 0; boardIndex < board.length; boardIndex++) {
+                for (int board2Index = 0; board2Index < board[0].length; board2Index++) {
+                    temp[boardIndex][board2Index] = board
+                }
+            }
+        }
+    }
 }
